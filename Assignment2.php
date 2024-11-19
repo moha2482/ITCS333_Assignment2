158a7a4ffbbb8d021e532f86fbb1615080d14f81
Mariam-Malik <161317785+Mariam-Malik@users.noreply.github.com>
2024-11-19 22:05:01 +0300

    API url

---
+++ Assignment2.php
@@ -0,0 +1,18 @@
+<!-- this is Done by Mariam-->
+<?php
+// API Endpoint URL
+$URL = "https://data.gov.bh/api/explore/v2.1/catalog/datasets/01-statistics-of-students-nationalities_updated/records?where=colleges%20like%20%22IT%22%20AND%20the_programs%20like%20%22bachelor%22&limit=100";
+
+// Fetch the data from the API
+$response = file_get_contents($URL);
+if ($response === FALSE) {
+    die("Error fetching data from API.");
+}
+
+// Decode JSON response into an associative array
+$data = json_decode($response, true);
+
+// Check if the data contains results
+if (!isset($data['results'])) {
+    die("No records found.");
+}
